ab18445ad1cc2d7cb620492c73aa51ba305957be
john1226 <devil.vergil1226@gmail.com>
2023-06-04 11:23:59 -0700

    deploy test

--- src/ApiWork/BaseUrl.js
+++ src/ApiWork/BaseUrl.js
@@ -3,11 +3,12 @@ import axios from "axios";
 const authToken = localStorage.getItem("token");
 const baseURL = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "http://135.181.168.31:8080",
+  baseURL: "https://5f0b-188-43-14-13.eu.ngrok.io",
 
   timeout: 80000,
   headers: {
     "x-access-token": authToken,
+    "Access-Control-Allow-Origin": "*",
   },
 });
 export default baseURL;
