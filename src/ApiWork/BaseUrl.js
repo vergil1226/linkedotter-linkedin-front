d71feb7108f06a2e3ac7cdb76a39f06b46a5a542
john1226 <devil.vergil1226@gmail.com>
2023-06-10 13:32:28 -0700

    base url changed

--- src/ApiWork/BaseUrl.js
+++ src/ApiWork/BaseUrl.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const authToken = localStorage.getItem("token");
 const baseURL = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://linkedin-messages-be.herokuapp.com/",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://linkedin-messages-be.herokuapp.com/",
 
   timeout: 300000,
   headers: {
